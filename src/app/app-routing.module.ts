686a9b3f1cf3ded5f271088ebf0e6d3b721a27ff
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-11 12:14:27 +0200

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SignUpComponent} from "./pages/sign-up/sign-up.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {ProductDetailsComponent} from "./pages/product-details/product-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'productDetails/:id',
+    component: ProductDetailsComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
