e80e7598168cb71cad089e8f33d36427ce741e58
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-11 21:07:27 +0200

    delete product component

---
+++ src/app/components/delete-product/delete-product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "../../data/interfaces/product";
+import {ProductService} from "../../core/services/ProductService/product.service";
+import {ProductRespondDTO} from "../../data/DTOs/product-respond-dto";
+
+@Component({
+  selector: 'app-delete-product',
+  templateUrl: './delete-product.component.html',
+  styleUrls: ['./delete-product.component.scss']
+})
+export class DeleteProductComponent implements OnInit {
+
+  constructor(private readonly productService: ProductService) { }
+  public productId="";
+  ngOnInit(): void {
+  }
+  deleteProduct(id: string) {
+    this.productService.deleteById(id).subscribe((data: Product) => {
+      console.log(data)
+    });
+  }
+}
