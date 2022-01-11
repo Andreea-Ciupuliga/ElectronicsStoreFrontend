35b14ae6567d2693d444e8d1e362af6b1746c23c
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-11 21:07:52 +0200

    add createProduct

--- src/app/core/services/ProductService/product.service.ts
+++ src/app/core/services/ProductService/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {ApiService} from "../api/api.service";
 import {Product} from "../../../data/interfaces/product";
+import {ProductRegisterDTO} from "../../../data/DTOs/product-register-dto";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +12,19 @@ export class ProductService {
   private readonly endpoint = '/Product';
   constructor(private apiService : ApiService) { }
 
-  getAllProducts(){ //merge
+  getAllProducts(){
     return this.apiService.get(this.endpoint + '/allProducts/');
   }
-  getProductByProductId(id: string){ //merge
+  getProductByProductId(id: string){
     return this.apiService.get<Product>(this.endpoint + '/byId/'+id);
   }
 
   deleteById(id: string){
     return this.apiService.delete(this.endpoint+'/delete/'+id);
+  }
+
+  createProduct(productRegisterDTO: ProductRegisterDTO) {
+    return this.apiService.post<Product>(this.endpoint + '/create',productRegisterDTO);
   }
 
 
